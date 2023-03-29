7345b65005b4c83478afce48fa416283885bfdbf
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-03-29 18:37:59 -0400

    commit

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom'
 
 import './App.css';
 
@@ -8,35 +9,44 @@ import Packages from './components/Packages'
 
 function App() {
 
-  const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
+  const packages = ['Activate your Crystals',
+    'Monkey Meditation',
+    'Soak in the Hotsprings',
+    'Hypnotherapy',
+    'Mineral Bath']
 
   return (
     <div className="App">
-      <header>
-        <h1 className="title">Welcome to Monty's Mineral SPA</h1>
+      <Router>
+        <header>
+          <h1 className="title">Welcome to Monty's Mineral SPA</h1>
 
-        <div className="navBar">
-          <ul>
-            <li>
-              <a href={<Home />}>Home</a>
-            </li>
-            <li>
-              <a href={<About />}>About Us</a>
-            </li>
-            <li>
-              <a href={<Packages packages={packages}/>}>Our Packages</a>
-            </li>
-          </ul>
+          <div className="navBar">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/packages">Our Packages</Link>
+              </li>
+            </ul>
+          </div>
+
+        </header>
+
+        <div className="display">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/packages" element={<Packages packages={packages} />} />
+          </Routes>
         </div>
-
-      </header>
+      </Router>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
